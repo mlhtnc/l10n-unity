305d90a56cc55aaf9c9cf7c3106aa22c3b6a884d
Melih Tunç <mlhtncc@gmail.com>
2023-11-15 14:00:47 +0300

    Create L10nComponent.cs

---
+++ L10nComponent.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class L10nComponent : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField]
+    private string key;
+
+    private TextMeshProUGUI text;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+
+        if(string.IsNullOrEmpty(key))
+        {
+            Debug.LogError($"You forgot to add key for {text.gameObject.name}");
+            return;
+        }
+
+        text.SetText(L10n.t[key]);
+
+        L10n.LanguageChanged += OnLanguageChanged;
+    }
+
+    #endregion
+
+    #region Callbacks
+
+    private void OnLanguageChanged()
+    {
+        text.SetText(L10n.t[key]);
+    }
+
+    #endregion
+}
